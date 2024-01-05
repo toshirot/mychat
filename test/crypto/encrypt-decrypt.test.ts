b9ce79e164053f0872a3e29f84a8e58a6aeccf57
Toshiro Takahashi <tato@reien.top>
2024-01-05 18:54:34 +0900

    PASS_PHRASE　を env にセットした

--- test/crypto/encrypt-decrypt.test.ts
+++ test/crypto/encrypt-decrypt.test.ts
@@ -5,14 +5,14 @@ const testMsg =
 `むかしむかしあるところにおじさんとおばさんが住んでいました
 むかしむかしあるところにおじさんとおばさんが住んでいました
 むかしむかしあるところにおじさんとおばさんが住んでいました`;
-const Passphrase = "thisismysalt";
+
+// あらかじめ環境変数に設定しておく
+const PASS_PHRASE = process.env.PASS_PHRASE
 
 describe('AESによる暗号化と復号化', () => {
 	it('AESで暗号化したものを復号化したら同じになる', () => {
-
-		const encryptedMsg = CryptoJS.AES.encrypt(CryptoJS.enc.Utf8.parse(testMsg), Passphrase).toString();
-		const decryptedMsg = CryptoJS.AES.decrypt(encryptedMsg, Passphrase).toString(CryptoJS.enc.Utf8);
+		const encryptedMsg = CryptoJS.AES.encrypt(CryptoJS.enc.Utf8.parse(testMsg), PASS_PHRASE).toString();
+		const decryptedMsg = CryptoJS.AES.decrypt(encryptedMsg, PASS_PHRASE).toString(CryptoJS.enc.Utf8);
 		expect(decryptedMsg).toEqual(testMsg)
 	})
-
 })
