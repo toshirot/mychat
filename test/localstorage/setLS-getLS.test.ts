b7bb47f230317daa110c9d0e087ccbb909c16755
Toshiro Takahashi <tato@reien.top>
2024-01-08 10:47:16 +0900

    test の localstorage に crypto と注意書き追加

--- test/localstorage/setLS-getLS.test.ts
+++ test/localstorage/setLS-getLS.test.ts
@@ -2,7 +2,8 @@
 // bun add -d @happy-dom/global-registrator
 // 
 import { describe, expect, it, test } from 'bun:test'
-
+import crypto from 'crypto';
+ 
 const getLS = (key:any):any => JSON.parse(localStorage.getItem(key) || '[]')
 const setLS = (key:any, val:any):any => {
   localStorage.setItem(key, JSON.stringify(val))
@@ -10,6 +11,7 @@ const setLS = (key:any, val:any):any => {
 
 describe('ブラウザDOM/ LocalStorage', () => {
     test('LocalStorageにセットしてからデータを取り出す', () => {
+        // 実際には thisismyPass を平文むき出しにしない
         const Passphrase = "thisismyPass";
         // ローカルストレージにセットする
         setLS('pass', Passphrase)
@@ -18,11 +20,12 @@ describe('ブラウザDOM/ LocalStorage', () => {
     })
 
     test('input要素からLocalStorageにセットしてデータを取り出す', () => {
+        // 実際には thisismyPass を平文むき出しにしない
         const Passphrase = "thisismyPass";
         document.body.innerHTML = `
             <input type="password" id="pass" name="pass" value="${Passphrase}">
             <script>
-                localStorage.setItem('pass',"thisismyPass");
+                localStorage.setItem('pass', encrypt("thisismyPass"));
             </script>
         `
         // input要素からローカルストレージにセットする
@@ -31,3 +34,15 @@ describe('ブラウザDOM/ LocalStorage', () => {
         expect(getLS('pass')).toStrictEqual(Passphrase)
     })
 })
+
+
+function encrypt(text: string, password: string) {
+    const cipher = crypto.createCipher('aes-256-cbc', password)
+    const crypted = cipher.update(text, 'utf-8', 'hex')
+    return crypted + cipher.final('hex')
+}
+function decrypt(text: string, password: string) {
+    const decipher = crypto.createDecipher('aes-256-cbc', password)
+    const decrypted = decipher.update(text, 'hex', 'utf-8')
+    return decrypted + decipher.final('utf-8')
+}
