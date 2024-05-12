5e9fc591fe40a09c42aa4e372258e39a3da12b55
Toshiro Takahashi <tato@reien.top>
2024-05-12 21:51:18 +0900

    Update index-9012.tsx

--- src/index-9012.tsx
+++ src/index-9012.tsx
@@ -38,9 +38,12 @@ const HOST = 'mychat.jp'
 // ポート HTTP と WebSocket 共通
 const PORT = 9012;
 const KEYS = {
+      cert: env.MYCHAT_CERT,
+      key: env.MYCHAT_PRIV
+     /*
    cert: Bun.file("/etc/letsencrypt/live/"+HOST+"/cert.pem"),
    key: Bun.file("/etc/letsencrypt/live/"+HOST+"/privkey.pem")
-   /* cert: ${{ secrets.MYCHAT_JP_CERT_PEM }},
+   cert: ${{ secrets.MYCHAT_JP_CERT_PEM }},
     key: ${{ secrets.MYCHAT_JP_PRIVEKEY_PEM }}*/
 }
 // ホームURL
